19754e13951579a1507370c8f36494a88c996b74
minh0518 <rubics@naver.com>
2023-02-06 20:42:19 +0900

    [#13]feat : 삭제 기능 구현 완료

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,7 +4,12 @@ export interface Composable {
   addChild(child: Component): void;
 }
 
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+type OnCloseListener = () => void;
+
+class PageItemComponent extends BaseComponent<HTMLElement> implements Composable{
+
+  private closeListener?: OnCloseListener;
+
   constructor() {
     super(`<li class="page_item">
             <section class="page_item_body"></section>
@@ -12,6 +17,12 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
               <button class="close">&times;</button>
             </div>
           </li>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener()
+      //closeListener가 있다면 호출
+      //누군가 등록을 해 놓았따면 close버튼이 클릭됐다고 알려줌
+    };
   }
 
   addChild(child: Component) {
@@ -20,9 +31,16 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
     //container의 afterbegin에다가 child의 this .element를 붙임
     //'afterbegin' >> element 안에 가장 첫번째 child
   }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
 }
 
-export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
+export class PageComponent
+  extends BaseComponent<HTMLUListElement>
+  implements Composable
+{
   constructor() {
     super('<ul class="page"></ul>');
   }
@@ -31,5 +49,11 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
+
+    //close버튼이 눌리면 실행될 함수 등록
+    item.setOnCloseListener(()=>{
+      item.removeFrom(this.element)
+      // this.element에서 item을 제거
+    })
   }
 }
